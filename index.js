41ad6d4ace95f4baeaa9e4147d11dc765b2b0a79
Collins Muriuki <murerwacollins@gmail.com>
2020-08-10 12:16:27 +0300

    2020-08-10_12-16

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const { google } = require("googleapis");
+const { google } = require('googleapis');
+const keys = require('./gs-api-test-7f8f4db8dd1c.json');
 
+const client = new google.auth.JWT(
+    keys.client_email,
+    null, 
+    keys.private_key, 
+    ["https://www.googleapis.com/auth/spreadsheets"],
+);
 
+client.authorize((err, tokens) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log("Connected");
+})
