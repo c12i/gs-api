2ea52c27866b05265923e5919481f920dd5a7ae3
Collins Muriuki <murerwacollins@gmail.com>
2020-08-10 20:42:29 +0300

    2020-08-10_20-42

--- index.ts
+++ index.ts
@@ -8,20 +8,22 @@ const run = async () => {
   await client.createSheet(s);
 }
 
+const dummyData = [
+  ["claim id", s],
+  ["patient name", "Collins Muriuki"],
+  ["member number", "10234H-H"],
+  ["invoice id", "321"],
+  ["diagnosis", ["Malaria", "Angular Stomatitis"].toString()],
+  ["prescription", ["Panadol", "1px", "od"].toString()],
+  ["invoiceTotal", "1000"],
+  ["item1", "200"],
+  ["item2", "300"],
+  ["item3", "250"],
+  ["item4", "250"],
+]
+
 const update = async () => {
-  await client.update(s, [
-    ["claim id", s],
-    ["patient name", "Collins Muriuki"],
-    ["member number", "10234H-H"],
-    ["invoice id", "321"],
-    ["diagnosis", ["Malaria", "Angular Stomatitis"].toString()],
-    ["prescription", ["Panadol", "1px", "od"].toString()],
-    ["invoiceTotal", "1000"],
-    ["item1", "200"],
-    ["item2", "300"],
-    ["item3", "250"],
-    ["item4", "250"],
-  ]);
+  await client.update(s, dummyData);
 }
 run();
 setTimeout(() => {
