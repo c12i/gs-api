5325a2e6afe40a14b528da40c483d28c3882ce80
Collins Muriuki <murerwacollins@gmail.com>
2020-08-10 14:33:06 +0300

    2020-08-10_14-33

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
-const { google } = require('googleapis');
-const keys = require('./gs-api-test-7f8f4db8dd1c.json');
+const { google } = require("googleapis");
+const keys = require("./gs-api-test-7f8f4db8dd1c.json");
 
-const client = new google.auth.JWT(
-    keys.client_email,
-    null, 
-    keys.private_key, 
-    ["https://www.googleapis.com/auth/spreadsheets"],
-);
+const client = new google.auth.JWT(keys.client_email, null, keys.private_key, [
+  "https://www.googleapis.com/auth/spreadsheets",
+]);
 
 client.authorize((err, tokens) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    gsRun(client);
-    console.log("Connected");
+  gsRun(client);
+  console.log("Connected");
 });
 
 const gsRun = async (cl) => {
-    const gsapi = google.sheets({version: "v4", auth: cl});
+  const gsapi = google.sheets({ version: "v4", auth: cl });
 
-    const options = {
-        spreadsheetId: "1ILfI5V5ZU9lt3Wqcr_gdRqDVqL2nymtQZNj8toF5EqQ",
-        range: "Data!A2:B5",
-    }
+  const options = {
+    spreadsheetId: "1ILfI5V5ZU9lt3Wqcr_gdRqDVqL2nymtQZNj8toF5EqQ",
+    range: "Data!A2:B5",
+  };
 
-    let response = await gsapi.spreadsheets.values.get(options);
+  let response = await gsapi.spreadsheets.values.get(options);
 
-    console.log(response.data.values);
-}
+  let dataArray = response.data.values;
+  dataArray.push([5, "Boom"]);
+  let newDataArray = dataArray;
+
+  const updateOptions = {
+    spreadsheetId: "1ILfI5V5ZU9lt3Wqcr_gdRqDVqL2nymtQZNj8toF5EqQ",
+    range: "Data!E2",
+    valueInputOption: "USER_ENTERED",
+    resource: { values: newDataArray },
+  };
+
+  let res = await gsapi.spreadsheets.values.update(updateOptions);
+
+  console.log(res);
+
+  let newResp = await gsapi.spreadsheets.batchUpdate({
+    requestBody: {
+      requests: [
+        {
+          addSheet: {
+            properties: { title: "Test" },
+          },
+        },
+      ],
+    },
+    spreadsheetId: "1ILfI5V5ZU9lt3Wqcr_gdRqDVqL2nymtQZNj8toF5EqQ"
+  });
+
+  console.log(newResp);
+};
