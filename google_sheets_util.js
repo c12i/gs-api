e577a39aa25078c87a18502810d0b7e1106f9b59
Collins Muriuki <murerwacollins@gmail.com>
2020-08-10 18:50:50 +0300

    2020-08-10_18-50

--- google_sheets_util.js
+++ google_sheets_util.js
@@ -2,6 +2,8 @@ const { google } = require("googleapis");
 const keys = require("./gs-api-test-7f8f4db8dd1c.json");
 
 class GoogleSheets {
+  spreadsheetId = "1ILfI5V5ZU9lt3Wqcr_gdRqDVqL2nymtQZNj8toF5EqQ";
+
   init() {
     const client = new google.auth.JWT(
       keys.client_email,
@@ -12,19 +14,60 @@ class GoogleSheets {
     return client;
   }
 
-  getValues(sheet) {
+  async createSheet(title) {
     const client = this.init();
     client.authorize((error, result) => {
       if(error) {
         return console.error(error);
       }
+      const gsapi = google.sheets({ version: "v4", auth: client });
+      let response = await gsapi.spreadsheets.batchUpdate({
+        requestBody: {
+          requests: [
+            {
+              addSheet: {
+                properties: { title },
+              },
+            },
+          ],
+        },
+        spreadsheetId: this.spreadsheetId
+      });
+      console.log(response);
+      return response;
+    })
+  }
 
+  async update(sheet, values) {
+    const client = this.init();
+    client.authorize((error, result) => {
+      if(error) {
+        return console.error(error);
+      }
+      const gsapi = google.sheets({ version: "v4", auth: client });
+      const updateOptions = {
+        spreadsheetId: this.spreadsheetId,
+        range: `${sheet}!E2`,
+        valueInputOption: "USER_ENTERED",
+        resource: { values },
+      };
+      let response = await gsapi.spreadsheets.values.update(updateOptions);
+      console.log(response);
+      return response;
+    })
+  }
+
+  async getValues(sheet) {
+    const client = this.init();
+    client.authorize((error, result) => {
+      if(error) {
+        return console.error(error);
+      }
       const gsapi = google.sheets({ version: "v4", auth: client });
       const options = {
-        spreadsheetId: "1ILfI5V5ZU9lt3Wqcr_gdRqDVqL2nymtQZNj8toF5EqQ",
+        spreadsheetId: this.spreadsheetId,
         range: `${sheet}!A2:B5`,
       };
-    
       let response = await gsapi.spreadsheets.values.get(options);
       console.log(response);
       return response;
