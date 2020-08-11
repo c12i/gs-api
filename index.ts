71ed805709eafe5913adba963306108fcb246869
Collins Muriuki <murerwacollins@gmail.com>
2020-08-11 19:27:01 +0300

    2020-08-11_19-27

--- index.ts
+++ index.ts
@@ -5,17 +5,16 @@ const client = new GoogleSheets("1ILfI5V5ZU9lt3Wqcr_gdRqDVqL2nymtQZNj8toF5EqQ");
 const rng = (Math.round(Math.random()*1000)).toString();
 
 const dummyData = [
-  ["claim id", rng],
-  ["patient name", "Collins Muriuki"],
-  ["member number", "10234H-H"],
-  ["invoice id", "321"],
-  ["diagnosis", ["Malaria", "Angular Stomatitis"].toString()],
-  ["prescription", ["Panadol", "1px", "od"].toString()],
-  ["invoiceTotal", "1000"],
+  ["id", rng],
+  ["name", "Collins Muriuki"],
+  ["number", "10234H-H"],
+  ["test id", "321"],
+  ["Languages", ["English", "German"].toString()],
   ["item1", "200"],
   ["item2", "300"],
   ["item3", "250"],
   ["item4", "250"],
+  ["total", "1000"],
 ]
 
 client.postToGoogleSheets(rng, dummyData);
